820477568b34a9d148bdda72ba48176deee75d61
Eduardo-C88 <eduardo.cristelo@gmail.com>
2025-12-02 09:46:05 +0000

    Swagger added-Documentation missing

--- auth-service/src/app.js
+++ auth-service/src/app.js
@@ -1,10 +1,21 @@
 const express = require('express');
 const routes = require('./routes/authRoutes');
+const bodyParser = require('body-parser');
+const path = require('path');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('../static/swagger/swagger.json');
 
 require('dotenv').config();
 
 const app = express();
-app.use(express.json());
+// Middleware to parse JSON bodies
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+// Serve static files from the 'static' directory
+app.use('/', express.static(path.join(__dirname, '../static')));
+//access api documentation
+app.use('/doc', express.static(path.join(__dirname, '../static/doc')));
+app.use('/apidoc', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use('/api/auth', routes);
 
