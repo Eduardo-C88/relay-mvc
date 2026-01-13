bf98cfce05c68ea8446bfb3f7a57120526150aee
Eduardo-C88 <eduardo.cristelo@gmail.com>
2026-01-13 20:02:07 +0000

    .

--- infrastructure/postgres/init_all.sql
+++
@@ -1,7 +0,0 @@
-CREATE DATABASE auth_db;
-CREATE DATABASE messaging_db;
-CREATE DATABASE notification_db;
-CREATE DATABASE resource_db;
-CREATE DATABASE transaction_db;
-CREATE DATABASE analytics_db;
-CREATE DATABASE review_db;
