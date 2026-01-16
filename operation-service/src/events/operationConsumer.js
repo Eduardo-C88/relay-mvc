22fb7202b5e6a7a965525623dd3fb3c905f4300f
Eduardo-C88 <eduardo.cristelo@gmail.com>
2026-01-16 14:06:37 +0000

    fix9?

--- operation-service/src/events/operationConsumer.js
+++ operation-service/src/events/operationConsumer.js
@@ -5,13 +5,13 @@ const purchasesService = require("../services/purchasesService");
 async function startOperationConsumers() {
   // Consume purchase requested events
   await consume(EVENTS.RESOURCE_RESERVED, async (purchase) => {
-    console.log(`Processing PurchaseRequested for resource ${purchase.payload.resourceId}`);
+    console.log(`Processing ResourceReserved for resource ${purchase.payload.resourceId}`);
     await purchasesService.resourceAvailable(purchase.payload.purchaseId, purchase.payload.ownerId);
   });
 
   // Consume Resource reservation failed events
   await consume(EVENTS.RESOURCE_RESERVATION_FAILED, async (purchase) => {
-    console.log(`Processing PurchaseRejected for resource ${purchase.payload.resourceId}`);
+    console.log(`Processing ResourceReservationFailed for resource ${purchase.payload.resourceId}`);
     await purchasesService.resourceFailed(purchase.payload.purchaseId);
   });
 }
